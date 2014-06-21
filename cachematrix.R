16539bd0de3261dfc84c9d947361de066d0fcad3
melsr <mhaynessr@gmail.com>
2014-06-21 15:41:12 -0400

    Second Commit with changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
+setwd("C:/Morrow/datasciencecoursera/R Programming/ProgrammingAssignment2")
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  }
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("Get cached data")
+    m
+  } else {
+    message("No cached data")
+    inverse_m <- solve(m)
+    inverse_m
+  }
 }
